f6723b0a17e0c00c36c29523e86ad1154435040e
Corey Carter <RC0042009@student.riversidecollege.ac.uk>
2020-09-16 10:55:08 +0100

    Will stop at border if you go to far right/left

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -19,7 +19,33 @@ namespace Move_Picture
 
         private void PictureBox1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+        {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+        }
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        { 
+                if (pictureBox1.Location.X > 0)
+              
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+    }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
         }
     }
 }
